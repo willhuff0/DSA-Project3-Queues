69f42c6aa8e51f5ec159f0da3faf054dc41861b8
William Huffman <wehuffman1@gmail.com>
2025-08-03 20:17:05 -0400

    Removed old example code

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,15 @@
 #include "Evaluation/JobSystem.h"
 #include "Evaluation/Jobs/Pools/DefaultJobPool.h"
 
-#include "Queues/OLD-BoundedRingBufferQueue.h"
 #include "Queues/StdQueueUnsafe.h"
 #include "Queues/StdQueueBlocking.h"
 
 int main() {
-    auto defaultJobPool = createDefaultJobPool();
+    std::vector<std::unique_ptr<Job>> jobPool;
+    createDefaultJobPool(jobPool);
 
     {
-        JobSystem<OLD<Job*>> jobSystem(defaultJobPool);
-        jobSystem.RunTest(4, 4, 10E6);
-    }
-    {
-        JobSystem<StdQueueBlocking<Job*>> jobSystem(defaultJobPool);
+        JobSystem<StdQueueBlocking<Job*>> jobSystem(jobPool);
         jobSystem.RunTest(4, 4, 10E6);
     }
 
