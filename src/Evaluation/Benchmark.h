3144c246c276bb9d73f736e805daea505ad561ac
FilmDragons <163951568+FilmDragons@users.noreply.github.com>
2025-08-04 16:00:26 -0400

    Adds comments for Benchmark.h

--- src/Evaluation/Benchmark.h
+++ src/Evaluation/Benchmark.h
@@ -11,6 +11,7 @@
 #include <vector>
 #include <chrono>
 
+// Structures for throughput and latency outputs
 struct ThroughputResult {
     size_t numJobsCompleted;
     std::chrono::high_resolution_clock::duration elapsed;
@@ -23,15 +24,19 @@ struct LatencyResult {
 template<typename QueueT>
 class Benchmark {
 public:
+    // When Benchmark is initialized, if there are no jobs, it will call createDefaultJobPool to create jobs
     explicit Benchmark() {
         if (jobs.empty()) {
             createDefaultJobPool(jobs);
         }
     }
 
+    // Finds the throughput of all the jobs
     ThroughputResult RunThroughput(size_t numJobs, int numProducers, int numConsumers) {
+        // Initializes stopwatch object
         Stopwatch stopwatch;
 
+        // Makes a job system
         auto jobSystem = std::make_unique<JobSystem<QueueT, false>>(jobs);
         jobSystem->StartWorkers(numProducers, numConsumers);
 
@@ -47,6 +52,7 @@ public:
         };
     }
 
+    // Finds the latency of all jobs
     LatencyResult RunLatency(size_t numJobs, int numProducers, int numConsumers) {
         auto jobSystem = std::make_unique<JobSystem<QueueT, true>>(jobs);
         jobSystem->StartWorkers(numProducers, numConsumers);
